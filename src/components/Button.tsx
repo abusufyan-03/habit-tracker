08d2a5ed25535d6e13a09a0b844f115a44307309
sufiyan <sufiyan.manuu@gmail.com>
2026-06-13 02:41:30 +0530

    refactor: make Button component reusable and variant-based

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,14 +1,31 @@
-import type { ReactNode } from "react";
+import type { ComponentProps} from "react";
+import { twMerge } from "tailwind-merge";
 
+type Variant = "primary" | "secondary" | "ghost-destructive"
 type ButtonProps = {
-    children: ReactNode;
-    disabled?: boolean;
-}
+    variant?: Variant
+} & ComponentProps<"button">
 
-export function Button ({children, disabled}: ButtonProps) {
+export function Button ({ variant ="primary", className, ...props}: ButtonProps) {
     return(
         <>
-        <button disabled={disabled} className="bg-zinc-200 hover:bg-zinc-400 text-zinc-900 px-2 py-1 rounded-lg font-semibold cursor-pointer transition-colors disabled:opacity-30 disabled:cursor-not-allowed disabled">{children}</button>
+        <button {...props} className={twMerge(
+            getVarientStyles(variant),
+            "px-2 py-1 rounded-lg cursor-pointer transition-colors disabled:opacity-30 disabled:cursor-not-allowed disabled",
+            className )}/>
         </>
     )
+}
+
+function getVarientStyles(variant: Variant) {
+    switch (variant) {
+        case "primary":
+            return "bg-zinc-200 hover:bg-zinc-400 text-zinc-900"
+        case "secondary":
+            return "bg-zinc-200 hover:bg-zinc-400 text-zinc-900"
+        case "ghost-destructive":
+            return "text-red-200 hover:text-red-200 hover:bg-red-800 bg-red-700"
+        default:
+            throw new Error(`Invalid varients: ${variant satisfies never}`);
+    }
 }
