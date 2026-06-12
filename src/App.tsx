1ebbf4111502cd66006758db70686b5fac431164
sufiyan <sufiyan.manuu@gmail.com>
2026-06-12 20:09:53 +0530

    Build Habit list component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { HabitForm } from "./components/HabitForm"
+import { HabitList } from "./components/HabitList"
 import { Header } from "./components/Header"
 
 export default function App() {
@@ -7,6 +8,7 @@ export default function App() {
     <div className="max-w-2xl mx-auto p-4 flex flex-col gap-4">
       <Header />
       <HabitForm/>
+      <HabitList/>
     </div>
     </>
   )
---
+++ src/components/HabitList.tsx
@@ -0,0 +1,64 @@
+// import { format } from "path";
+// import { format } from "path";
+import { Button } from "./Button";
+import { eachDayOfInterval, endOfWeek, startOfWeek, format, isFuture } from "date-fns"
+
+export function HabitList() {
+  let habit = [
+    { id: "1", name: "Practice Javascript" },
+    { id: "2", name: "Learn Angular" },
+  ];
+
+  if (habit.length === 0) {
+    return (
+      <p className="text-zinc-500 text-center py-12">
+        No Habits yet. Add one above to get started.
+      </p>
+    );
+  }
+
+  return (
+    <>
+      {habit.map((habit) => (
+        <HabitItem key={habit.id} habit={habit} />
+      ))}
+    </>
+  );
+}
+
+type HabitItemProps = {
+  habit: { id: string; name: string };
+};
+
+function HabitItem({ habit }: HabitItemProps) {
+  const visibleDates = eachDayOfInterval({
+    start: startOfWeek(new Date(), {weekStartsOn: 1}),
+    end: endOfWeek(new Date(), {weekStartsOn: 1})
+  })
+
+
+  return (
+    <>
+      <div className="bg-zinc-800 p-4 rounded-xl flex flex-col gap-3">
+        {/* Item List Header with Delete Btn */}
+        <div className="flex items-center justify-between ">
+          <div className="flex gap-3">
+            <span className="text-xl font-bold">{habit.name}</span>
+            <span>🔥3</span>
+          </div>
+          <Button>Delete</Button>
+        </div>
+
+        {/* Item list bottom with week days */}
+        <div className="flex gap-1.5">
+          {visibleDates.map((date) => (
+            <Button key={date.toISOString()} disabled={isFuture(date)}>
+              <span className="font-medium">{format(date, "EEE")}</span>
+              <span>{format(date, "d")}</span>
+            </Button>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
