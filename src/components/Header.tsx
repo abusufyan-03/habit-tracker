1f9c85d561d8d81dad0613de299117c8f14c009c
sufiyan <sufiyan.manuu@gmail.com>
2026-06-26 19:58:55 +0530

    feat: display dynamic daily habit completion count using habit context

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,17 @@
+import { isToday } from "date-fns";
+import { useHabits } from "../context/useHabits";
 import { Button } from "./Button";
 
 export function Header() {
+  const {habits} = useHabits();
+
+  const doneToday = habits.filter(h => h.completion.some(c => isToday(c))).length;
   return (
     <>
   <header className="flex justify-between">
     <div className="flex flex-col gap-1">
       <h1 className="text-3xl font-bold text-zinc-200">Habit Tracker</h1>
-      <span className="text-sm text-zinc-400">1/1 done today</span>
+      <span className="text-sm text-zinc-400">{doneToday}/{habits.length} done today</span>
     </div>
 
     <div className="flex flex-col gap-1 items-center">
